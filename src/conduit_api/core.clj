b313cc6898c8d73c8ebc2b52731ccd0fa5b3a0a5
Lucas Angelino <lucas.angelino@gmail.com>
2018-01-29 21:02:11 -0200

    Configure Compojure lib

--- src/conduit_api/core.clj
+++ src/conduit_api/core.clj
@@ -1,10 +1,19 @@
 (ns conduit-api.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
 
-(defn app [req]
+(defn hello-world [req]
   {:status 200
    :headers {}
    :body "It's running!!!"})
+
+(defroutes routes
+  (GET "/" [] hello-world)
+  (not-found "Page not found!"))
+
+(def app 
+  routes)
 
 (defn -main [port]
   (jetty/run-jetty app {:port (Integer. port)}))
