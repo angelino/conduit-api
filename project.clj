38033355df1cc195fb39a0677ffd27f9698fa6c4
Lucas Angelino <lucas.angelino@gmail.com>
2018-01-29 22:46:52 -0200

    Configure wrap-json-reponse middleware and create an endpoint api for login

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.3"]
+                 [ring/ring-json "0.4.0"]
                  [compojure "1.6.0"]]
   :main conduit-api.core
   :profiles {:dev {:source-paths ["src" "dev"]
--- src/conduit_api/core.clj
+++ src/conduit_api/core.clj
@@ -1,6 +1,7 @@
 (ns conduit-api.core
   (:require [ring.adapter.jetty :as jetty]
-            [compojure.core :refer [defroutes GET]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [not-found]]))
 
 (defn hello-world [req]
@@ -8,12 +9,24 @@
    :headers {}
    :body "It's running!!!"})
 
+(defn handle-login [req]
+  {:status 200
+   :headers {}
+   :body {:user {:email "jake@jake.com"
+                 :token "jwt.token.here"
+                 :username "jake"
+                 :bio "I work at statefarm"
+                 :image ""}}})
+
 (defroutes routes
   (GET "/" [] hello-world)
+
+  (POST "/api/users/login" [] handle-login)
   (not-found "Page not found!"))
 
 (def app 
-  routes)
+  (-> routes
+      wrap-json-response))
 
 (defn -main [port]
   (jetty/run-jetty app {:port (Integer. port)}))
