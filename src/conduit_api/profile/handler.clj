4d94b7b49c97d16c59a8b337292bfcac6ed94877
Lucas Angelino <lucas.angelino@gmail.com>
2018-01-30 23:44:04 -0200

    Refactor to use response helper

--- src/conduit_api/profile/handler.clj
+++ src/conduit_api/profile/handler.clj
@@ -1,25 +1,20 @@
-(ns conduit-api.profile.handler)
+(ns conduit-api.profile.handler
+  (:require [ring.util.response :refer [response]]))
 
 (defn handle-profile [req]
-  {:status 200
-   :headers {}
-   :body {:profile {:username "jake"
-                    :bio "I work at statefarm"
-                    :image "https://static.productionready.io/images/smiley-cyrus.jpg"
-                    :fallowing false}}})
+  (response {:profile {:username "jake"
+                       :bio "I work at statefarm"
+                       :image "https://static.productionready.io/images/smiley-cyrus.jpg"
+                       :fallowing false}}))
 
 (defn handle-follow [req]
-  {:status 200
-   :headers {}
-   :body {:profile {:username "jake"
-                    :bio "I work at statefarm"
-                    :image "https://static.productionready.io/images/smiley-cyrus.jpg"
-                    :fallowing true}}})
+  (response {:profile {:username "jake"
+                       :bio "I work at statefarm"
+                       :image "https://static.productionready.io/images/smiley-cyrus.jpg"
+                       :fallowing false}}))
 
 (defn handle-unfollow [req]
-  {:status 200
-   :headers {}
-   :body {:profile {:username "jake"
-                    :bio "I work at statefarm"
-                    :image "https://static.productionready.io/images/smiley-cyrus.jpg"
-                    :fallowing false}}})
+  (response {:profile {:username "jake"
+                       :bio "I work at statefarm"
+                       :image "https://static.productionready.io/images/smiley-cyrus.jpg"
+                       :fallowing false}}))
